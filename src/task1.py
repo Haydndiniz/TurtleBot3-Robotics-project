0d07dbec4ee4fe85baac441127c85d47b659e987
Haydn Diniz <hadiniz1@sheffiled.ac.uk>
2021-04-22 16:01:42 +0100

    adding base code to task 1

---
+++ src/task1.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+import rospy
+from sensor_msgs.msg import LaserScan
+from geometry_msgs.msg import Twist
+
+def callback(dt):
+
+    move.linear.x = 0.2 # go forward (linear velocity)
+    move.angular.z = 0.0 # rotate (angular velocity)
+
+    pub.publish(move)
+
+
+move = Twist()
+pub = rospy.Publisher("/cmd_vel", Twist, queue_size=10)
+sub = rospy.Subscriber("/scan", LaserScan, callback)
+
+rospy.spin() # Loops infinitely until someone stops the program execution
