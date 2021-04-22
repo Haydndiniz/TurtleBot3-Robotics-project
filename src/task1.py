0613970671bed0afc0e73daf80feddacfb52147a
Haydn Diniz <hadiniz1@sheffiled.ac.uk>
2021-04-22 16:43:07 +0100

    basic navigation

--- src/task1.py
+++ src/task1.py
@@ -5,8 +5,17 @@ from geometry_msgs.msg import Twist
 
 def callback(dt):
 
-    move.linear.x = 0.2 # go forward (linear velocity)
-    move.angular.z = 0.0 # rotate (angular velocity)
+    distance_1 = 0.5
+    distance_2 = 0.5
+
+    if dt.ranges[0]>distance_1 and dt.ranges[20]>distance_2 and dt.ranges[340]>distance_2:
+        # move forward
+        move.linear.x = 0.2 # go forward (linear velocity) m/s
+        move.angular.z = 0.0 # rotate (angular velocity)
+    else:
+        # turns left
+        move.linear.x = 0.0 # go forward (linear velocity) m/s
+        move.angular.z = 0.2
 
     pub.publish(move)
 
